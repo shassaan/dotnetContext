3aae345e630ba2126df52ebaa5ed807b629da369
shassaan <hassan.ahmed@eonhealth.com>
2021-06-08 17:19:04 +0500

    [main] add build tools

--- context.cli/Program.cs
+++ context.cli/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using Health.Direct.Context;
+using MimeKit;
+using MimeKit.Utils;
 
 namespace context.cli
 {
@@ -6,7 +12,81 @@ namespace context.cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //
+            // Context
+            //
+            var contextBuilder = new ContextBuilder();
+
+            contextBuilder
+                .WithContentId(MimeUtils.GenerateMessageId())
+                .WithDisposition("metadata.txt")
+                .WithTransferEncoding(ContentEncoding.Base64)
+                .WithVersion("1.0")
+                .WithId(MimeUtils.GenerateMessageId())
+                .WithPatientId(
+                    new PatientInstance
+                    {
+                        PidContext = "2.16.840.1.113883.19.999999",
+                        LocalPatientId = "123456"
+                    }.ToList()
+                )
+                .WithType(ContextStandard.Type.CategoryRadiology, ContextStandard.Type.ActionReport)
+                .WithPurpose(ContextStandard.Purpose.PurposeResearch)
+                .WithPatient(
+                    new Patient
+                    {
+                        GivenName = "John",
+                        SurName = "Doe",
+                        MiddleName = "Jacob",
+                        DateOfBirth = "1961-12-31",
+                        Gender = "M",
+                        PostalCode = "12345"
+                    }
+                );
+
+            var context = contextBuilder.Build();
+
+            //
+            // Mime message and simple body
+            //
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("HoboJoe", "hobojoe@hsm.DirectInt.lab"));
+            message.To.Add(new MailboxAddress("Toby", "toby@redmond.hsgincubator.com"));
+            message.Subject = "Sample message with pdf and context attached";
+            message.Headers.Add(MailStandard.Headers.DirectContext, context.ContentId);
+
+            var body = new TextPart("plain")
+            {
+                Text = @"Simple Body"
+            };
+
+            //
+            // Represent PDF attachment
+            // 
+            var pdf = new MimePart("application/pdf")
+            {
+                ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                FileName = "report.pdf",
+                ContentTransferEncoding = ContentEncoding.Base64
+            };
+
+            var byteArray = Encoding.UTF8.GetBytes("Fake PDF (invalid)");
+            var stream = new MemoryStream(byteArray);
+            pdf.Content = new MimeContent(stream);
+
+            //
+            // Multi part construction
+            //
+            var multiPart = new Multipart("mixed")
+            {
+                body,
+                contextBuilder.BuildMimePart(),
+                pdf
+            };
+
+            message.Body = multiPart;
+            Console.WriteLine(JsonSerializer.Serialize(message.DirectContext().Metadata));
+
         }
     }
 }
